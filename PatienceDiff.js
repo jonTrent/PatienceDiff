848151f1b2ce695dde6eaae7b08bc24fc949d579
jonTrent <31544186+jonTrent@users.noreply.github.com>
2022-08-14 11:10:18 -0400

    Update PatienceDiff.js
    Tweaked proposed edits in function patienceDiffPlus to properly align declaration and use of variable "lastLineCountMoved".
    
    Also, used https://zz85.github.io/mrdoobapproves/ to make code more readable.

--- PatienceDiff.js
+++ PatienceDiff.js
@@ -3,7 +3,7 @@
  * author: Jonathan Trent
  * version: 2.0
  *
- * use:  patienceDiff( aLines[], bLines[], diffPlusFlag)
+ * use:  patienceDiff( aLines[], bLines[], diffPlusFlag )
  *
  * where:
  *      aLines[] contains the original text lines.
@@ -14,256 +14,349 @@
  * returns an object with the following properties:
  *      lines[] with properties of:
  *          line containing the line of text from aLines or bLines.
- *          aIndex referencing the index in aLine[].
+ *          aIndex referencing the index in aLines[].
  *          bIndex referencing the index in bLines[].
  *              (Note:  The line is text from either aLines or bLines, with aIndex and bIndex
  *               referencing the original index. If aIndex === -1 then the line is new from bLines,
  *               and if bIndex === -1 then the line is old from aLines.)
- *          moved is true if the line was moved from elsewhere in aLines[] or bLines[].
  *      lineCountDeleted is the number of lines from aLines[] not appearing in bLines[].
  *      lineCountInserted is the number of lines from bLines[] not appearing in aLines[].
- *      lineCountMoved is the number of lines moved outside of the Longest Common Subsequence.
+ *      lineCountMoved is 0. (Only set when using patienceDiffPlus.)
  *
  */
 
-function patienceDiff(aLines, bLines, diffPlusFlag) {
-  //
-  // findUnique finds all unique values in arr[lo..hi], inclusive.  This
-  // function is used in preparation for determining the longest common
-  // subsequence.  Specifically, it first reduces the array range in question
-  // to unique values.
-  //
-  // Returns an ordered Map, with the arr[i] value as the Map key and the
-  // array index i as the Map value.
-  //
-  function findUnique(arr, lo, hi) {
-    const lineMap = new Map();
+function patienceDiff( aLines, bLines, diffPlusFlag ) {
 
-    for (let i = lo; i <= hi; i++) {
-      let line = arr[i];
-      if (lineMap.has(line)) {
-        lineMap.get(line).count++;
-        lineMap.get(line).index = i;
-      } else {
-        lineMap.set(line, { count: 1, index: i });
-      }
-    }
+	//
+	// findUnique finds all unique values in arr[lo..hi], inclusive.  This
+	// function is used in preparation for determining the longest common
+	// subsequence.  Specifically, it first reduces the array range in question
+	// to unique values.
+	//
+	// Returns an ordered Map, with the arr[i] value as the Map key and the
+	// array index i as the Map value.
+	//
+  
+	function findUnique( arr, lo, hi ) {
 
-    lineMap.forEach((val, key, map) => {
-      if (val.count !== 1) {
-        map.delete(key);
-      } else {
-        map.set(key, val.index);
-      }
-    });
+		const lineMap = new Map();
 
-    return lineMap;
-  }
+		for ( let i = lo; i <= hi; i ++ ) {
 
-  //
-  // uniqueCommon finds all the unique common entries between aArray[aLo..aHi]
-  // and bArray[bLo..bHi], inclusive.  This function uses findUnique to pare
-  // down the aArray and bArray ranges first, before then walking the comparison
-  // between the two arrays.
-  //
-  // Returns an ordered Map, with the Map key as the common line between aArray
-  // and bArray, with the Map value as an object containing the array indexes of
-  // the matching unique lines.
-  //
-  function uniqueCommon(aArray, aLo, aHi, bArray, bLo, bHi) {
-    let ma = findUnique(aArray, aLo, aHi);
-    let mb = findUnique(bArray, bLo, bHi);
+			let line = arr[ i ];
+      
+			if ( lineMap.has( line ) ) {
 
-    ma.forEach((val, key, map) => {
-      if (mb.has(key)) {
-        map.set(key, { indexA: val, indexB: mb.get(key) });
-      } else {
-        map.delete(key);
-      }
-    });
+				lineMap.get( line ).count ++;
+				lineMap.get( line ).index = i;
 
-    return ma;
-  }
+			} else {
 
-  //
-  // longestCommonSubsequence takes an ordered Map from the function uniqueCommon
-  // and determines the Longest Common Subsequence (LCS).
-  //
-  // Returns an ordered array of objects containing the array indexes of the
-  // matching lines for a LCS.
-  //
-  function longestCommonSubsequence(abMap) {
-    const ja = [];
+				lineMap.set( line, {
+          count: 1,
+          index: i
+        } );
 
-    // First, walk the list creating the jagged array.
-    abMap.forEach((val, key, map) => {
-      let i = 0;
-      while (ja[i] && ja[i][ja[i].length - 1].indexB < val.indexB) {
-        i++;
-      }
+			}
 
-      if (!ja[i]) {
-        ja[i] = [];
-      }
+		}
 
-      if (0 < i) {
-        val.prev = ja[i - 1][ja[i - 1].length - 1];
-      }
+		lineMap.forEach( ( val, key, map ) => {
 
-      ja[i].push(val);
-    });
+			if ( val.count !== 1 ) {
 
-    // Now, pull out the longest common subsequence.
-    let lcs = [];
-    if (0 < ja.length) {
-      let n = ja.length - 1;
-      lcs = [ja[n][ja[n].length - 1]];
-      while (lcs[lcs.length - 1].prev) {
-        lcs.push(lcs[lcs.length - 1].prev);
-      }
-    }
+				map.delete( key );
 
-    return lcs.reverse();
-  }
+			} else {
 
-  // "result" is the array used to accumulate the aLines that are deleted, the
-  // lines that are shared between aLines and bLines, and the bLines that were
-  // inserted.
-  const result = [];
-  let deleted = 0;
-  let inserted = 0;
+				map.set( key, val.index );
 
-  // aMove and bMove will contain the lines that don't match, and will be returned
-  // for possible searching of lines that moved.
+			}
 
-  const aMove = [];
-  const aMoveIndex = [];
-  const bMove = [];
-  const bMoveIndex = [];
+		} );
 
-  //
-  // addToResult simply pushes the latest value onto the "result" array.  This
-  // array captures the diff of the line, aIndex, and bIndex from the aLines
-  // and bLines array.
-  //
-  function addToResult(aIndex, bIndex) {
-    if (bIndex < 0) {
-      aMove.push(aLines[aIndex]);
-      aMoveIndex.push(result.length);
-      deleted++;
-    } else if (aIndex < 0) {
-      bMove.push(bLines[bIndex]);
-      bMoveIndex.push(result.length);
-      inserted++;
-    }
+		return lineMap;
 
-    result.push({
-      line: 0 <= aIndex ? aLines[aIndex] : bLines[bIndex],
-      aIndex: aIndex,
-      bIndex: bIndex,
-    });
-  }
+	}
 
-  //
-  // addSubMatch handles the lines between a pair of entries in the LCS.  Thus,
-  // this function might recursively call recurseLCS to further match the lines
-  // between aLines and bLines.
-  //
-  function addSubMatch(aLo, aHi, bLo, bHi) {
-    // Match any lines at the beginning of aLines and bLines.
-    while (aLo <= aHi && bLo <= bHi && aLines[aLo] === bLines[bLo]) {
-      addToResult(aLo++, bLo++);
-    }
+	//
+	// uniqueCommon finds all the unique common entries between aArray[aLo..aHi]
+	// and bArray[bLo..bHi], inclusive.  This function uses findUnique to pare
+	// down the aArray and bArray ranges first, before then walking the comparison
+	// between the two arrays.
+	//
+	// Returns an ordered Map, with the Map key as the common line between aArray
+	// and bArray, with the Map value as an object containing the array indexes of
+	// the matching unique lines.
+	//
+  
+	function uniqueCommon( aArray, aLo, aHi, bArray, bLo, bHi ) {
 
-    // Match any lines at the end of aLines and bLines, but don't place them
-    // in the "result" array just yet, as the lines between these matches at
-    // the beginning and the end need to be analyzed first.
-    let aHiTemp = aHi;
-    while (aLo <= aHi && bLo <= bHi && aLines[aHi] === bLines[bHi]) {
-      aHi--;
-      bHi--;
-    }
+		const ma = findUnique( aArray, aLo, aHi );
+		const mb = findUnique( bArray, bLo, bHi );
 
-    // Now, check to determine with the remaining lines in the subsequence
-    // whether there are any unique common lines between aLines and bLines.
-    //
-    // If not, add the subsequence to the result (all aLines having been
-    // deleted, and all bLines having been inserted).
-    //
-    // If there are unique common lines between aLines and bLines, then let's
-    // recursively perform the patience diff on the subsequence.
-    let uniqueCommonMap = uniqueCommon(aLines, aLo, aHi, bLines, bLo, bHi);
-    if (uniqueCommonMap.size === 0) {
-      while (aLo <= aHi) {
-        addToResult(aLo++, -1);
-      }
-      while (bLo <= bHi) {
-        addToResult(-1, bLo++);
-      }
-    } else {
-      recurseLCS(aLo, aHi, bLo, bHi, uniqueCommonMap);
-    }
+		ma.forEach( ( val, key, map ) => {
 
-    // Finally, let's add the matches at the end to the result.
-    while (aHi < aHiTemp) {
-      addToResult(++aHi, ++bHi);
-    }
-  }
+			if ( mb.has( key ) ) {
 
-  //
-  // recurseLCS finds the longest common subsequence (LCS) between the arrays
-  // aLines[aLo..aHi] and bLines[bLo..bHi] inclusive.  Then for each subsequence
-  // recursively performs another LCS search (via addSubMatch), until there are
-  // none found, at which point the subsequence is dumped to the result.
-  //
-  function recurseLCS(aLo, aHi, bLo, bHi, uniqueCommonMap) {
-    const x = longestCommonSubsequence(
-      uniqueCommonMap || uniqueCommon(aLines, aLo, aHi, bLines, bLo, bHi)
-    );
-    if (x.length === 0) {
-      addSubMatch(aLo, aHi, bLo, bHi);
-    } else {
-      if (aLo < x[0].indexA || bLo < x[0].indexB) {
-        addSubMatch(aLo, x[0].indexA - 1, bLo, x[0].indexB - 1);
-      }
+				map.set( key, {
+					indexA: val,
+					indexB: mb.get( key )
+				} );
 
-      let i;
-      for (i = 0; i < x.length - 1; i++) {
-        addSubMatch(
-          x[i].indexA,
-          x[i + 1].indexA - 1,
-          x[i].indexB,
-          x[i + 1].indexB - 1
-        );
-      }
+			} else {
 
-      if (x[i].indexA <= aHi || x[i].indexB <= bHi) {
-        addSubMatch(x[i].indexA, aHi, x[i].indexB, bHi);
-      }
-    }
-  }
+				map.delete( key );
 
-  recurseLCS(0, aLines.length - 1, 0, bLines.length - 1);
+			}
 
-  if (diffPlusFlag) {
-    return {
-      lines: result,
-      lineCountDeleted: deleted,
-      lineCountInserted: inserted,
-      lineCountMoved: 0,
-      aMove: aMove,
-      aMoveIndex: aMoveIndex,
-      bMove: bMove,
-      bMoveIndex: bMoveIndex,
-    };
-  }
+		} );
 
-  return {
-    lines: result,
-    lineCountDeleted: deleted,
-    lineCountInserted: inserted,
-    lineCountMoved: 0,
-  };
+		return ma;
+
+	}
+
+	//
+	// longestCommonSubsequence takes an ordered Map from the function uniqueCommon
+	// and determines the Longest Common Subsequence (LCS).
+	//
+	// Returns an ordered array of objects containing the array indexes of the
+	// matching lines for a LCS.
+	//
+  
+	function longestCommonSubsequence( abMap ) {
+
+		const ja = [];
+
+		// First, walk the list creating the jagged array.
+    
+		abMap.forEach( ( val, key, map ) => {
+
+			let i = 0;
+      
+			while ( ja[ i ] && ja[ i ][ ja[ i ].length - 1 ].indexB < val.indexB ) {
+
+				i ++;
+
+			}
+
+			if ( ! ja[ i ] ) {
+
+				ja[ i ] = [];
+
+			}
+
+			if ( 0 < i ) {
+
+				val.prev = ja[ i - 1 ][ ja[ i - 1 ].length - 1 ];
+
+			}
+
+			ja[ i ].push( val );
+
+		} );
+
+		// Now, pull out the longest common subsequence.
+    
+		let lcs = [];
+    
+		if ( 0 < ja.length ) {
+
+			let n = ja.length - 1;
+			lcs = [ ja[ n ][ ja[ n ].length - 1 ] ];
+      
+			while ( lcs[ lcs.length - 1 ].prev ) {
+
+				lcs.push( lcs[ lcs.length - 1 ].prev );
+
+			}
+
+		}
+
+		return lcs.reverse();
+
+	}
+
+	// "result" is the array used to accumulate the aLines that are deleted, the
+	// lines that are shared between aLines and bLines, and the bLines that were
+	// inserted.
+  
+	const result = [];
+	let deleted = 0;
+	let inserted = 0;
+
+	// aMove and bMove will contain the lines that don't match, and will be returned
+	// for possible searching of lines that moved.
+  
+	const aMove = [];
+	const aMoveIndex = [];
+	const bMove = [];
+	const bMoveIndex = [];
+
+	//
+	// addToResult simply pushes the latest value onto the "result" array.  This
+	// array captures the diff of the line, aIndex, and bIndex from the aLines
+	// and bLines array.
+	//
+  
+	function addToResult( aIndex, bIndex ) {
+
+		if ( bIndex < 0 ) {
+
+			aMove.push( aLines[ aIndex ] );
+			aMoveIndex.push( result.length );
+			deleted ++;
+
+		} else if ( aIndex < 0 ) {
+
+			bMove.push( bLines[ bIndex ] );
+			bMoveIndex.push( result.length );
+			inserted ++;
+
+		}
+
+		result.push( {
+			line: 0 <= aIndex ? aLines[ aIndex ] : bLines[ bIndex ],
+			aIndex: aIndex,
+			bIndex: bIndex,
+		} );
+
+	}
+
+	//
+	// addSubMatch handles the lines between a pair of entries in the LCS.  Thus,
+	// this function might recursively call recurseLCS to further match the lines
+	// between aLines and bLines.
+	//
+  
+	function addSubMatch( aLo, aHi, bLo, bHi ) {
+
+		// Match any lines at the beginning of aLines and bLines.
+    
+		while ( aLo <= aHi && bLo <= bHi && aLines[ aLo ] === bLines[ bLo ] ) {
+
+			addToResult( aLo ++, bLo ++ );
+
+		}
+
+		// Match any lines at the end of aLines and bLines, but don't place them
+		// in the "result" array just yet, as the lines between these matches at
+		// the beginning and the end need to be analyzed first.
+    
+		let aHiTemp = aHi;
+    
+		while ( aLo <= aHi && bLo <= bHi && aLines[ aHi ] === bLines[ bHi ] ) {
+
+			aHi --;
+			bHi --;
+
+		}
+
+		// Now, check to determine with the remaining lines in the subsequence
+		// whether there are any unique common lines between aLines and bLines.
+		//
+		// If not, add the subsequence to the result (all aLines having been
+		// deleted, and all bLines having been inserted).
+		//
+		// If there are unique common lines between aLines and bLines, then let's
+		// recursively perform the patience diff on the subsequence.
+    
+		const uniqueCommonMap = uniqueCommon( aLines, aLo, aHi, bLines, bLo, bHi );
+    
+		if ( uniqueCommonMap.size === 0 ) {
+
+			while ( aLo <= aHi ) {
+
+				addToResult( aLo ++, - 1 );
+
+			}
+
+			while ( bLo <= bHi ) {
+
+				addToResult( - 1, bLo ++ );
+
+			}
+
+		} else {
+
+			recurseLCS( aLo, aHi, bLo, bHi, uniqueCommonMap );
+
+		}
+
+		// Finally, let's add the matches at the end to the result.
+    
+		while ( aHi < aHiTemp ) {
+
+			addToResult( ++ aHi, ++ bHi );
+
+		}
+
+	}
+
+	//
+	// recurseLCS finds the longest common subsequence (LCS) between the arrays
+	// aLines[aLo..aHi] and bLines[bLo..bHi] inclusive.  Then for each subsequence
+	// recursively performs another LCS search (via addSubMatch), until there are
+	// none found, at which point the subsequence is dumped to the result.
+	//
+  
+	function recurseLCS( aLo, aHi, bLo, bHi, uniqueCommonMap ) {
+
+		const x = longestCommonSubsequence( uniqueCommonMap || uniqueCommon( aLines, aLo, aHi, bLines, bLo, bHi ) );
+    
+		if ( x.length === 0 ) {
+
+			addSubMatch( aLo, aHi, bLo, bHi );
+
+		} else {
+
+			if ( aLo < x[ 0 ].indexA || bLo < x[ 0 ].indexB ) {
+
+				addSubMatch( aLo, x[ 0 ].indexA - 1, bLo, x[ 0 ].indexB - 1 );
+
+			}
+
+			let i;
+			for ( i = 0; i < x.length - 1; i ++ ) {
+
+				addSubMatch( x[ i ].indexA, x[ i + 1 ].indexA - 1, x[ i ].indexB,	x[ i + 1 ].indexB - 1 );
+
+			}
+
+			if ( x[ i ].indexA <= aHi || x[ i ].indexB <= bHi ) {
+
+				addSubMatch( x[ i ].indexA, aHi, x[ i ].indexB, bHi );
+
+			}
+
+		}
+
+	}
+
+	recurseLCS( 0, aLines.length - 1, 0, bLines.length - 1 );
+
+	if ( diffPlusFlag ) {
+
+		return {
+			lines: result,
+			lineCountDeleted: deleted,
+			lineCountInserted: inserted,
+			lineCountMoved: 0,
+			aMove: aMove,
+			aMoveIndex: aMoveIndex,
+			bMove: bMove,
+			bMoveIndex: bMoveIndex,
+		};
+
+	}
+
+	return {
+		lines: result,
+		lineCountDeleted: deleted,
+		lineCountInserted: inserted,
+		lineCountMoved: 0,
+	};
+
 }
 
 /**
@@ -288,55 +381,69 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
  *          moved is true if the line was moved from elsewhere in aLines[] or bLines[].
  *      lineCountDeleted is the number of lines from aLines[] not appearing in bLines[].
  *      lineCountInserted is the number of lines from bLines[] not appearing in aLines[].
- *      lineCountMoved is the number of lines moved outside of the Longest Common Subsequence.
+ *      lineCountMoved is the number of lines that moved.
  *
  */
 
-function patienceDiffPlus(aLines, bLines) {
-  let difference = patienceDiff(aLines, bLines, true);
+function patienceDiffPlus( aLines, bLines ) {
 
-  let aMoveNext = difference.aMove;
-  let aMoveIndexNext = difference.aMoveIndex;
-  let bMoveNext = difference.bMove;
-  let bMoveIndexNext = difference.bMoveIndex;
+	const difference = patienceDiff( aLines, bLines, true );
 
-  delete difference.aMove;
-  delete difference.aMoveIndex;
-  delete difference.bMove;
-  delete difference.bMoveIndex;
+	let aMoveNext = difference.aMove;
+	let aMoveIndexNext = difference.aMoveIndex;
+	let bMoveNext = difference.bMove;
+	let bMoveIndexNext = difference.bMoveIndex;
 
-  const lastLineCountMoved = difference.lineCountMoved;
+	delete difference.aMove;
+	delete difference.aMoveIndex;
+	delete difference.bMove;
+	delete difference.bMoveIndex;
+  
+  let lastLineCountMoved;
 
-  do {
-    let aMove = aMoveNext;
-    let aMoveIndex = aMoveIndexNext;
-    let bMove = bMoveNext;
-    let bMoveIndex = bMoveIndexNext;
+	do {
 
-    aMoveNext = [];
-    aMoveIndexNext = [];
-    bMoveNext = [];
-    bMoveIndexNext = [];
+		let aMove = aMoveNext;
+		let aMoveIndex = aMoveIndexNext;
+		let bMove = bMoveNext;
+		let bMoveIndex = bMoveIndexNext;
 
-    let subDiff = patienceDiff(aMove, bMove);
+		aMoveNext = [];
+		aMoveIndexNext = [];
+		bMoveNext = [];
+		bMoveIndexNext = [];
 
-    subDiff.lines.forEach((v, i) => {
-      if (0 <= v.aIndex && 0 <= v.bIndex) {
-        difference.lines[aMoveIndex[v.aIndex]].moved = true;
-        difference.lines[bMoveIndex[v.bIndex]].aIndex = aMoveIndex[v.aIndex];
-        difference.lines[bMoveIndex[v.bIndex]].moved = true;
-        difference.lineCountInserted--;
-        difference.lineCountDeleted--;
-        difference.lineCountMoved++;
-      } else if (v.bIndex < 0) {
-        aMoveNext.push(aMove[v.aIndex]);
-        aMoveIndexNext.push(aMoveIndex[v.aIndex]);
-      } else {
-        bMoveNext.push(bMove[v.bIndex]);
-        bMoveIndexNext.push(bMoveIndex[v.bIndex]);
-      }
-    });
-  } while (0 < difference.lineCountMoved - lastLineCountMoved);
+		let subDiff = patienceDiff( aMove, bMove );
+    
+    lastLineCountMoved = difference.lineCountMoved;
 
-  return difference;
+		subDiff.lines.forEach( ( v, i ) => {
+
+			if ( 0 <= v.aIndex && 0 <= v.bIndex ) {
+
+				difference.lines[ aMoveIndex[ v.aIndex ] ].moved = true;
+				difference.lines[ bMoveIndex[ v.bIndex ] ].aIndex = aMoveIndex[ v.aIndex ];
+				difference.lines[ bMoveIndex[ v.bIndex ] ].moved = true;
+				difference.lineCountInserted --;
+				difference.lineCountDeleted --;
+				difference.lineCountMoved ++;
+
+			} else if ( v.bIndex < 0 ) {
+
+				aMoveNext.push( aMove[ v.aIndex ] );
+				aMoveIndexNext.push( aMoveIndex[ v.aIndex ] );
+
+			} else {
+
+				bMoveNext.push( bMove[ v.bIndex ] );
+				bMoveIndexNext.push( bMoveIndex[ v.bIndex ] );
+
+			}
+
+		} );
+
+	} while ( 0 < difference.lineCountMoved - lastLineCountMoved );
+
+	return difference;
+
 }
